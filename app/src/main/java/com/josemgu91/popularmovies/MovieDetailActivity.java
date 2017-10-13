de9c7a70fc11597cbd10303564cc48f4578be8c3
José Miguel García Urrutia <josemgu91@gmail.com>
2017-10-13 15:57:41 -0500

    MainActivity renamed to MoviesOverviewActivity. MovieDetailActivity created. Layout resources created. Picasso library added.

---
+++ app/src/main/java/com/josemgu91/popularmovies/MovieDetailActivity.java
@@ -0,0 +1,10 @@
+package com.josemgu91.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jose on 10/13/17.
+ */
+
+public class MovieDetailActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/josemgu91/popularmovies/MoviesOverviewActivity.java
+++ app/src/main/java/com/josemgu91/popularmovies/MoviesOverviewActivity.java
@@ -3,11 +3,11 @@ package com.josemgu91.popularmovies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MoviesOverviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movies_overview);
     }
 }
