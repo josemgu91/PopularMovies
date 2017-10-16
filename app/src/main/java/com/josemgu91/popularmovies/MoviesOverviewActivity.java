01fbd4f8cebb7b35c53a3c4f7dce3f0fc4c5c8f6
José Miguel García Urrutia <josemgu91@gmail.com>
2017-10-15 23:58:25 -0500

    Movies overview screen layout, menu and basic style added.

--- app/src/main/java/com/josemgu91/popularmovies/MoviesOverviewActivity.java
+++ app/src/main/java/com/josemgu91/popularmovies/MoviesOverviewActivity.java
@@ -2,6 +2,8 @@ package com.josemgu91.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MoviesOverviewActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MoviesOverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_overview);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.movies_overview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.action_order_most_popular:
+                item.setChecked(true);
+                return true;
+            case R.id.action_order_top_rated:
+                item.setChecked(true);
+                return true;
+            default:
+                return false;
+        }
     }
 }
